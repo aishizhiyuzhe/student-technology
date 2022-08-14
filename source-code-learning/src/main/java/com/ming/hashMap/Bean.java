02ba0f436089fa242b5c0c08b79f57c835019634
aishizhiyuzhe@163.com <aishizhiyuzhe@2>
2022-08-14 20:59:08 +0800

    map学习

--- source-code-learning/src/main/java/com/ming/hashMap/Bean.java
+++ source-code-learning/src/main/java/com/ming/hashMap/Bean.java
@@ -3,12 +3,16 @@ package com.ming.hashMap;
 public class Bean {
     private int i;
 
+    private int hashCode=-1;
     public Bean(int i) {
         this.i = i;
     }
 
     @Override
     public int hashCode() {
-        return (int)(Math.random()*100+1);
+        if (hashCode==-1){
+            hashCode=(int)(Math.random()*64+1);
+        }
+        return hashCode;
     }
 }
--- source-code-learning/src/main/java/com/ming/hashMap/HashMapLearning.java
+++ source-code-learning/src/main/java/com/ming/hashMap/HashMapLearning.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 
 public class HashMapLearning {
     public static void main(String[] args) {
-        HashMap hashMap=new HashMap();
-        for (Integer i=0;i<200;i++){
+        HashMap<Bean,Integer> hashMap=new HashMap();
+        Bean[] beans=new Bean[400];
+        for (Integer i=0;i<400;i++){
             //创建一个对象是因为想要使得hashCode一致
-            Bean bean=new Bean(i);
-            System.out.println(bean.hashCode());
-            hashMap.put(bean,i);
+            beans[i]=new Bean(i);
+            System.out.println(i+"-----------------"+beans[i].hashCode());
+            hashMap.put(beans[i],i);
         }
+
+        hashMap.remove(beans[100]);
+
     }
 }
